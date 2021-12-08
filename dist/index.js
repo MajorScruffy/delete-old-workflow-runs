c6ae328cec58160d27a1318c546a821a1927c1b7
Stefan Mihai Stanescu <s.stanescu@outlook.com>
2021-12-08 19:33:03 +0100

    Filter using actor

--- dist/index.js
+++ dist/index.js
@@ -50661,8 +50661,37 @@ async function main() {
     const createdBeforeDate = new Date(createdBefore)
     core.info(`Created before date ${createdBeforeDate}`);
 
+    const actor = core.getInput('actor');
+    core.info(`Created before string ${actor}`);
+
+    const branch = core.getInput('branch');
+    core.info(`Created before string ${branch}`);
+
+    const event = core.getInput('event');
+    core.info(`Created before string ${event}`);
+
+    const status = core.getInput('status');
+    core.info(`Created before string ${status}`);
+
     let parameters = [];
+    parameters["per_page"] = 50;
     parameters["page"] = 0;
+
+    if(!!actor){
+      parameters["actor"] = actor;
+    }
+
+    if(!!branch){
+      parameters["branch"] = branch;
+    }
+
+    if(!!event){
+      parameters["event"] = event;
+    }
+
+    if(!!status){
+      parameters["status"] = status;
+    }
 
     for(;;) {
       parameters["page"]++;
@@ -50689,38 +50718,56 @@ async function main() {
       for (const workflowRun of data.workflow_runs) {
         const createdAt = new Date(workflowRun.created_at);
 
-        if(createdBeforeDate < createdAt){
-          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}`);
+        if(!!createdBeforeDate && createdBeforeDate < createdAt){
+          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id} - created`);
+          continue;
+        }
+
+        if(!!actor && workflowRun.head_commit.author.email != actor){
+          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id} - actor`);
+          continue;
+        }
+
+        if(!!branch && workflowRun.head_branch != branch){
+          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id} - branch`);
+          continue;
+        }
+
+        if(!!event && workflowRun.event != event){
+          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id} - event`);
+          continue;
+        }
+
+        if(!!status && workflowRun.status != status){
+          core.info(`Skipped workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id} - status`);
           continue;
         }
 
         core.info(`Deleting workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}`);
 
-        if(workflowRun.head_commit.message == "Followed documentation to create JS action"){
-          let deleteParameters = [];
-          deleteParameters["run_id"] = 0;
+        let deleteParameters = [];
+        deleteParameters["run_id"] = 0;
 
-          let requestOptions = octokit.request.endpoint(
-            `DELETE /repos/${repository}/actions/runs/${workflowRun.id}`,
-            deleteParameters
-          );
+        let requestOptions = octokit.request.endpoint(
+          `DELETE /repos/${repository}/actions/runs/${workflowRun.id}`,
+          deleteParameters
+        );
 
-          core.info(`parsed request options: ${inspect(requestOptions)}`);
+        core.info(`parsed request options: ${inspect(requestOptions)}`);
 
-          let { status, headers, data } = await octokit.request(requestOptions);
+        let { status, headers, data } = await octokit.request(requestOptions);
 
-          core.info(`< ${status} ${Date.now() - time}ms`);
-          core.info(inspect(headers));
+        core.info(`< ${status} ${Date.now() - time}ms`);
+        core.info(inspect(headers));
 
-          if(status == 204){
-            core.info(`Deleted workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}`);
-          }
-          else{
-            core.warning(`Something went wrong while deleting workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}. Status code: ${status}`);
-          }
+        if(status == 204){
+          core.info(`Deleted workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}`);
+        }
+        else{
+          core.warning(`Something went wrong while deleting workflow "${workflowRun.head_commit.message}" with ID:${workflowRun.id}. Status code: ${status}`);
         }
       }
-    } 
+    }
   } catch (error) {
     if (error.status) {
       core.info(`< ${error.status} ${Date.now() - time}ms`);
