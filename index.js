684695c855a100dde89b99f40aa7d5c10283b951
Stefan Mihai Stanescu <s.stanescu@outlook.com>
2021-12-08 17:55:40 +0100

    Use Octokit to call GitHub API

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/action");
 
-try {
-  // `repository-name` input defined in action.yml
-  const repositoryName = core.getInput('repository-name');
-  console.log(`Hello ${repositoryName}!`);
+main();
 
-  // `access-token` input defined in action.yml
-  const accessToken = core.getInput('access-token');
-  console.log(`Hello ${accessToken}!`);
+async function main() {
+  const time = Date.now();
 
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  try {
+    const octokit = new Octokit();
+
+    // `repository-name` input defined in action.yml
+    const repositoryName = core.getInput('repository-name');
+    core.info(repositoryName);
+
+    // `access-token` input defined in action.yml
+    const accessToken = core.getInput('access-token');
+
+    // workaround for https://github.com/octokit/request-action/issues/71
+    // un-encode "repo" in /repos/{repo} URL when "repo" parameter is set to ${{ github.repository }}
+    const { url, body, ...options } = octokit.request.endpoint(
+      "GET /repos/MajorScruffy/delete-old-workflow-runs/actions/runs",
+      []
+    );
+    const requestOptions = {
+      ...options,
+      data: body,
+      url: url.replace(
+        /\/repos\/([^/]+)/,
+        (_, match) => "/repos/" + decodeURIComponent(match)
+      ),
+    };
+
+    core.debug(`route: ${inspect(route)}`);
+    core.debug(`parameters: ${inspect(parameters)}`);
+    core.debug(`parsed request options: ${inspect(requestOptions)}`);
+
+    const { status, headers, data } = await octokit.request(requestOptions);
+
+    core.info(`< ${status} ${Date.now() - time}ms`);
+    core.info(headers);
+    core.info(data);
+
+    core.setOutput("status", status);
+  } catch (error) {
+    if (error.status) {
+      core.info(`< ${error.status} ${Date.now() - time}ms`);
+    }
+
+    core.setOutput("status", error.status);
+    core.debug(inspect(error));
+    core.setFailed(error.message);
+  }
 }
